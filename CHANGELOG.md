4de9c9d0b11d4db106cb9567a1bd5008d9175bc7
matheexe <matheus07bb@outlook.com>
2025-02-27 20:51:00 -0300

    Conclusão da Calculadora.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,19 @@
 - Estrutura do HTML.
 - Estilização do CSS.
 
+
 ## [Versão 0.0.8] - 2025-02-26
 ### Adicionado
 - JavaScript.
 - Funcionamento da tabela.
+
+
+## [Versão 0.1.1] - 2025-02-26
+### Adicionado
+- Matérias e notas.
+- Calculo da média das notas.
+- Nota mínima para aprovação.
+- Reprovado ou aprovado.
+
+### Melhorado
+- Display da tabela.
--- main.js
+++ main.js
@@ -1,28 +1,76 @@
 const form = document.getElementById('form');
 const emojiAprovado = '<i class="fa-solid fa-face-laugh-beam"></i>';
 const emojiReprovado = '<i class="fa-solid fa-face-frown"></i>';
+const mediaFinal = document.getElementById('mediafinal');
+const resultado = document.getElementById('textoResultado');
+const notaMinima = '';
+const atividades = [];
+const notas = [];
 let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+    calculoFinal();
+    
+    
+});
+
+function adicionaLinha(){
     const inputNomeAtividade = document.getElementById('textArea')
     const inputNota = document.getElementById('numberArea');
+    const notaMinima = parseFloat(document.getElementById('notaMinima').value)
 
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNota.value}</td>`;
-    linha += `<td>${inputNota.value >= 7 ? emojiAprovado : emojiReprovado}</td>`;
-    linha += '</tr>';
 
-    linhas += linha;
+    if(atividades.includes(inputNomeAtividade.value)){
+        alert(`A atividade ${inputNomeAtividade.value} já foi inserida.`);
+    } else{
 
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNota.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNota.value}</td>`;
+        linha += `<td>${inputNota.value >= notaMinima ? emojiAprovado : emojiReprovado}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
+
+
 
     inputNomeAtividade.value = '';
     inputNota.value = '';
+}
 
 
-});
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+
+function calculoFinal(){
+    let somaNotas = 0;
+    const notaMinima = parseFloat(document.getElementById('notaMinima').value)
+    
+    for (let i = 0; i < notas.length; i++){
+        somaNotas += notas[i];
+    }
+
+    const media = somaNotas / notas.length;
+    mediaFinal.innerHTML = media;
+    
+    if(media >= notaMinima){
+        resultado.innerHTML = ('Aprovado');
+        resultado.style.backgroundColor = ('#86d52e');
+    } else{
+        resultado.innerHTML = ('Reprovado');
+        resultado.style.backgroundColor = ('#cb4f42');
+    }
+
+}
 
